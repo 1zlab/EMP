f905ba8a71ded7eb3b72d7d212311c6ecc24d952
How <Fuermohao@outlook.com>
2018-09-24 12:26:13 +0800

    fixed: donot print wifi record!

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # EMP(Easy MicroPython) is a upy module to make things Easy on MicroPython.
+
+> 该项目的目的在于通过对代码的封装,使得用户能够更容易的在MicroPython上进行一些基本的操作
+目前版本为0.1.10
--- emp_wifi.py
+++ emp_wifi.py
@@ -109,7 +109,7 @@ class Wifi():
                 records = cls.get_records().insert(0, default)
             else:
                 records = cls.get_records()
-            print(records)
+            # print(records)
             networks = [i.get('essid') for i in worker.scan()]
             for i in records:
                 if i[0] in networks:
